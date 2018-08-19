63af908ead19520991846844b304aae30a8e8e7a
usermi12 <misiek.opala@gmail.com>
2018-08-19 20:55:53 +0200

    ostatniezmiany

--- src/skrypt.js
+++ src/skrypt.js
@@ -1,18 +1,14 @@
 let tura = 0;
 let p1 = 0;
 let p2 = 0;
-let mustang=12;
-console.log(mustang);
-function ustaw() {
-  let reset = document.getElementById("reset");
-  let pierwszy = document.getElementById("wynikpierwszy");
-  let drugi = document.getElementById("wynikdrugi");
-  reset.innerHTML = "nowa gra?";
- 
+
+function ustaw(a,b,c) {
+
+  a.innerHTML = "nowa gra?";
   let l = "gr1:" + p1;
   let pr = "gr2:" + p2;
-  pierwszy.innerHTML = l;
-  drugi.innerHTML = pr;
+  b.innerHTML = l;
+  c.innerHTML = pr;
 }
 function operacja()
 {
@@ -41,12 +37,7 @@ function operacja2()
     let r = "gr2:" + p2;
     drugi.innerHTML = r;
 }
-function koniecgry() {
-
-    let gra=document.getElementById("gra");
-    let reset=document.getElementById("reset");
-    let pierwszy = document.getElementById("wynikpierwszy");
-    let drugi = document.getElementById("wynikdrugi");
+function koniecgry(a,b,c,d) {
     let licznik =1;
     for (let i=1;i<=9;i++)
     {
@@ -71,89 +62,20 @@ if (tura%9==0)
     reset.innerHTML = "jeszcze raz?";
 }
 
-    if (text1=='o' && text2=='o' && text3=='o')
-    {
-      operacja();
-    }
-
-    else if (text4 == 'o' && text5 == 'o' && text6 == 'o') {
+    if ((text1 == "o" && text2 == "o" && text3 == "o") || (text4 == "o" && text5 == "o" && text6 == "o") || (text7 == "o" && text8 == "o" && text9 == "o") || (text1 == "o" && text4 == "o" && text7 == "o") || (text2 == "o" && text5 == "o" && text8 == "o") || (text3 == "o" && text6 == "o" && text9 == "o") || (text1 == "o" && text5 == "o" && text9 == "o") || (text3 == "o" && text5 == "o" && text7 == "o")) {
         operacja();
     }
 
-    else if (text7 == 'o' && text8 == 'o' && text9 == 'o') {
-        operacja();
-    }
 
-    else if (text1 == 'o' && text4 == 'o' && text7 == 'o') {
-        operacja();
-    }
-
-    else if (text1 == 'o' && text4 == 'o' && text7 == 'o') {
-        operacja();
-    }
-
-    else if (text2 == 'o' && text5 == 'o' && text8 == 'o') {
-        operacja();
-    }
-
-    else if (text3 == 'o' && text6 == 'o' && text9 == 'o') {
-        operacja();
-    }
-  
-    else if (text1=='o' && text5=='o' && text9=='o')
-    {
-        operacja();
-    }
-
-    else if (text3=='o' && text5=='o' && text7=='o')
-    {
-        operacja();
-    }
-
-    /////////////////////////////gracz2
-    if (text1 == 'x' && text2 == 'x' && text3 == 'x') {
-       operacja2();
-    }
-
-    else if (text4 == 'x' && text5 == 'x' && text6 == 'x') {
-        operacja2();
-    }
-
-    else if (text7 == 'x' && text8 == 'x' && text9 == 'x') {
-        operacja2();
-    }
-
-    else if (text1 == 'x' && text4 == 'x' && text7 == 'x') {
-        operacja2();
-    }
-
-    else if (text1 == 'x' && text4 == 'x' && text7 == 'x') {
-        operacja2();
-    }
-
-    else if (text2 == 'x' && text5 == 'x' && text8 == 'x') {
-        operacja2();
-    }
-
-    else if (text3 == 'x' && text6 == 'x' && text9 == 'x') {
-        operacja2();
-    }
-
-    else if (text1 == 'x' && text5 == 'x' && text9 == 'x')
-    {
-        operacja2();
-    }
-
-    else if (text3 == 'x' && text5 == 'x' && text7 == 'x') {
+    if ((text1 == "x" && text2 == "x" && text3 == "x") || (text4 == "x" && text5 == "x" && text6 == "x") || (text7 == "x" && text8 == "x" && text9 == "x") || (text1 == "x" && text4 == "x" && text7 == "x") || (text2 == "x" && text5 == "x" && text8 == "x") || (text3 == "x" && text6 == "x" && text9 == "x") || (text1 == "x" && text5 == "x" && text9 == "x") || (text3 == "x" && text5 == "x" && text7 == "x")) {
         operacja2();
     }
     
 }
 
 
-function reset() {
-    let reset=document.getElementById("reset");
-    let gra=document.getElementById("gra");
+function Reset() {
+   
     tura=0;
     if (reset.innerHTML=='jeszcze raz?')
     {
@@ -169,22 +91,28 @@ function reset() {
   }
 }
 function klik(id) {
-  
+    let gra = document.getElementById("gra");
+    let reset = document.getElementById("reset");
+    let pierwszy = document.getElementById("wynikpierwszy");
+    let drugi = document.getElementById("wynikdrugi");
     let x = document.getElementById(id);
   x.style.background = "lightgray";
   x.style.cursor = "default";
   if (x.innerHTML === "" && tura % 2 == 0) {
     x.innerHTML = "o";
     tura++;
-      koniecgry();
+      koniecgry(gra,reset,pierwszy,drugi);
   } else if (x.innerHTML === "" && tura % 2 == 1) {
     x.innerHTML = "x";
     tura++;
-      koniecgry();
+      koniecgry(gra, reset, pierwszy, drugi);
   }
 
 }
 
 function start() {
-  ustaw();
+    let reset = document.getElementById("reset");
+    let pierwszy = document.getElementById("wynikpierwszy");
+    let drugi = document.getElementById("wynikdrugi");
+  ustaw(reset,pierwszy,drugi);
 }
